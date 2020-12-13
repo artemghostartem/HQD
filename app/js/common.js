5efde9341a9d8313af9abdf114310d37206ccb00
Davyd Tevzadze <pawtod20@gmail.com>
2020-12-13 10:27:10 +0200

    final edits

--- app/js/common.js
+++ app/js/common.js
@@ -1,22 +1,18 @@
 $(function() {
     //buscet number-counter
-    function numbering() {
-    	$('.minus').click(function() {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-	    });
-	    $('.plus').click(function() {
-	        var $input = $(this).parent().find('input');
-	        $input.val(parseInt($input.val()) + 1);
-	        $input.change();
-	        return false;
-	    });
-    }
-    
+    function numbering(fruct) {
+        let fructNext = `${fruct}-next`;
+        $(fructNext).on('click', function () {
+           $(this).prev().val(+$(this).prev().val() + 1);
+           // console.log('hi')
+        });
+
+        let fructPrev = `${fruct}-prev`;
+        $(fructPrev).on('click', function () {
+           if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1);
+           // console.log('hi')
+        });
+    }    
 
     //buscet removing
     function removingProducts(fruct) {
@@ -56,9 +52,17 @@ $(function() {
             let inputNumber = $(cloneProductCart).find('.numberInput')
             inputNumber.attr("name", fruct);
             
+
+            let minusInput = $(cloneProductCart).find('.qty-minus')
+            minusInput.addClass(`${fruct}-prev`)
+
+            let plusInput = $(cloneProductCart).find('.qty-plus')
+            plusInput.addClass(`${fruct}-next`)
+
 	        productCart.after(cloneProductCart)
 
     }
+
     //popup after "buy" click
     function onBuyPopup() {
         let popup = document.querySelector('.popup-on-buy')
@@ -72,39 +76,16 @@ $(function() {
         
     }
 
-
-    //total prive
-    // let totalprice = 0
-    // function getTotalPrice() {
-    //     let prices = document.querySelectorAll('.form-product-right .price')
-    //     let totalPricePlace = $('.totalprice')
-    //     for( i = 0 ; i < prices.length; i++) {
-            
-    //         totalprice = +totalprice + +prices[i].textContent
-            
-            
-    //     }
-    //     totalPricePlace.html(totalprice);
-    //     totalprice = 0
-    //     console.log(totalprice)
-    // }
-
-
     //pagescroll2id
     $("a[rel='m_PageScroll2id']").mPageScroll2id({
         offset: function(){
             return offsetCustom()
         }
     });
+
     function offsetCustom() {
         let header = document.querySelector('header')
-        let offset
-        // if (window.innerWidth > 768) {
-        //     offset = header.offsetHeight-1
-        // } else {
-        //     offset = 0 
-        // }
-        offset = header.offsetHeight-1
+        let offset = header.offsetHeight-1
         return offset;
     }
 
@@ -148,39 +129,91 @@ $(function() {
     })
 
     $('.products-main-element:nth-child(1) .button').on('click', function() {
-        if ($('.golubika').length <1 ) {
-        	updateBuscet('HQD Cuvie - Blueberry', '500', 'img/Blueberry.png', 'golubika');
-            // getTotalPrice()	
+        if ($('.mango').length <1 ) {
+        	updateBuscet('HQD Cuvie - Iced mango', '199', 'img/mango.png', 'mango');
         } 
         removingProducts();
-        numbering();
+        numbering('.mango');
         
     })
     $('.products-main-element:nth-child(2) .button').on('click', function() {
-        if ($('.klubnika').length <1 ) {
-        	updateBuscet('HQD Cuvie - Strawberry', '500', 'img/strawberry.png', 'klubnika');
-            // getTotalPrice()	
+        if ($('.strawberry').length <1 ) {
+        	updateBuscet('HQD Cuvie - Strawberry', '199', 'img/strawberry.png', 'strawberry');
         } 
         removingProducts();
-        numbering();
+        numbering('.strawberry');
         
     })
     $('.products-main-element:nth-child(3) .button').on('click', function() {
-        if ($('.myata').length <1 ) {
-        	updateBuscet('HQD Cuvie - Mint', '500', 'img/mint.png', 'myata');	
+        if ($('.tobacco').length <1 ) {
+        	updateBuscet('HQD Cuvie - Nut tobacco', '199', 'img/nuts.png', 'tobacco');	
             // getTotalPrice()
         } 
         removingProducts();
-        numbering();
+        numbering('.tobacco');
         
     })
     $('.products-main-element:nth-child(4) .button').on('click', function() {
-        if ($('.ananas').length <1 ) {
-        	updateBuscet('HQD Cuvie - Pienapple', '500', 'img/pienapple.png', 'ananas');
-             // getTotalPrice()	
+        if ($('.Blueberry').length <1 ) {
+        	updateBuscet('HQD Cuvie - Blueberry', '199', 'img/blueberry.png', 'Blueberry');
+        
         } 
         removingProducts();
-        numbering();
+        numbering('.Blueberry');
+       
+    })
+    $('.products-main-element:nth-child(5) .button').on('click', function() {
+        if ($('.cola').length <1 ) {
+            updateBuscet('HQD Cuvie - Cola iced', '199', 'img/cola.png', 'cola');
+        
+        } 
+        removingProducts();
+        numbering('.cola');
+       
+    })
+    $('.products-main-element:nth-child(6) .button').on('click', function() {
+        if ($('.peach').length <1 ) {
+            updateBuscet('HQD Cuvie - Peach iced', '199', 'img/ice_peach.png', 'peach');
+        
+        } 
+        removingProducts();
+        numbering('.peach');
+       
+    })
+    $('.products-main-element:nth-child(7) .button').on('click', function() {
+        if ($('.gum').length <1 ) {
+            updateBuscet('HQD Cuvie - Bubble gum', '199', 'img/bubble.png', 'gum');
+        
+        } 
+        removingProducts();
+        numbering('.gum');
+       
+    })
+     $('.products-main-element:nth-child(8) .button').on('click', function() {
+        if ($('.mint').length <1 ) {
+            updateBuscet('HQD Cuvie - Ice mint', '199', 'img/mint.png', 'mint');
+        
+        } 
+        removingProducts();
+        numbering('.mint');
+       
+    })
+      $('.products-main-element:nth-child(9) .button').on('click', function() {
+        if ($('.mixed').length <1 ) {
+            updateBuscet('HQD Cuvie - Mixed fruit', '199', 'img/mixed.png', 'mixed');
+        
+        } 
+        removingProducts();
+        numbering('.mixed');
+       
+    })
+       $('.products-main-element:nth-child(10) .button').on('click', function() {
+        if ($('.grapey').length <1 ) {
+            updateBuscet('HQD Cuvie - Grapey', '199', 'img/grape.png', 'grapey');
+        
+        } 
+        removingProducts();
+        numbering('.grapey');
        
     })
 
--- app/telegram.php
+++ app/telegram.php
@@ -5,25 +5,39 @@
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$blueberry = $_POST['golubika'];
-$strawberry = $_POST['klubnika'];
-$pienapple = $_POST['myata'];
-$mite = $_POST['ananas'];
+$mango = $_POST['mango'];
+$strawberry = $_POST['strawberry'];
+$tobacco = $_POST['tobacco'];
+$Blueberry = $_POST['Blueberry'];
+$cola = $_POST['cola'];
+$peach = $_POST['peach'];
+$gum = $_POST['gum'];
+$mint = $_POST['mint'];
+$mixed = $_POST['mixed'];
+$grapey = $_POST['grapey'];
+
 
 $token = "1413574047:AAGdYU2waomx4N4u8ctSGM06wqOpDxSumI4";
 $chat_id = "-412533262";
 $arr = array(
   'Имя: ' => $name,
   'Телефон: ' => $phone,
-  'Голубика: ' => $blueberry,
+  'Манго: ' => $mango, 
   'Клубника: ' => $strawberry,
-  'Ананас: ' => $pienapple,
-  'Мята: ' => $mite,
-
+  'Мускат: ' => $tobacco,
+  'Голубика: ' => $Blueberry,
+  'Кола со льдом: ' => $cola,
+  'Персик: ' => $peach,
+  'Жевачка: ' => $gum,
+  'Мята: ' => $mint,
+  'Фруктовый микс: ' => $mixed,
+  'Виноград: ' => $grapey,
 );
 
 foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
+  if (isset($value)) {
+  	$txt .= "<b>".$key."</b> ".$value."%0A";
+  }
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
